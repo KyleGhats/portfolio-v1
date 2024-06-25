15e0137ff0bd1f5e900d66527ebef10f115912f8
Kyle Gatudan <kylegatudan@gmail.com>
2024-06-25 16:18:40 +0800

    metadata updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,16 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: new URL('https://www.kylegatudan.com/'),
   title: "Kyle Gatudan",
   description: "Website Portfolio",
+  openGraph: {
+    title: 'Kyle Gatudan',
+    description: 'Website Portfolio',
+    url: 'https://www.kylegatudan.com/',
+    images: '/projects/personal.png',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({ children }) {
