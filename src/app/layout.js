68c349bc9f5c9f2f2fa7db8a51d13be1f579d227
Kyle Gatudan <kylegatudan@seabudigital.com>
2024-06-30 02:14:39 +0800

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />  
+      </body>
     </html>
   );
 }
